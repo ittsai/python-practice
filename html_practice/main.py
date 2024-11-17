729ba72f516563a842a806a990859af941311b28
I-Ting <it.tsai.1113@gmail.com>
2024-11-17 17:17:06 +0100

    add api + html practice

---
+++ html_practice/main.py
@@ -0,0 +1,32 @@
+# web scraping, beautiful soup
+from bs4 import BeautifulSoup
+import requests
+STATIC_WEBSITE = "https://appbrewery.github.io/news.ycombinator.com/"
+
+response = requests.get(STATIC_WEBSITE)
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+
+# Title
+print(soup.title)
+
+# article
+article_tag = soup.find(name="a", class_="storylink")
+print(article_tag.getText())
+article_link = article_tag.get("href")
+print(article_link)
+article_upvotes = [score.getText() for score in soup.find_all(name="span", class_="score")]
+print(article_upvotes)
+# with open("website.html") as file:#
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title.string)
+# print(soup.prettify())
+# print(soup.findAll(name="a"))
+
+# for tag in soup.findAll(name="a"):
+#     print(tag.get("href"))
+#
+# company_url = soup.select_one(selector="p a")
