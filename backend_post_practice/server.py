b2d51eaba1489ad1fdc7ca8bd9ea4d7f556e4083
I-Ting <it.tsai.1113@gmail.com>
2024-11-19 11:54:23 +0100

    add flask_wtf practice

--- backend_post_practice/server.py
+++ backend_post_practice/server.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import DataRequired, Email, number_range
 
 app = Flask(__name__)
+app.secret_key = "qwertyu"
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired(), Email(),number_range(6,120)])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+
+    return render_template("login.html", form=login_form)
 
 @app.route("/click", methods=["POST"])
 def click():
