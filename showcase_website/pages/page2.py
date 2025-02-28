438d465df933487f51b529b6a7f8bd8f0ac285ab
I-Ting <it.tsai.1113@gmail.com>
2025-02-28 13:48:44 +0100

    add email function

--- showcase_website/pages/page2.py
+++ showcase_website/pages/page2.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from  send_emails import send_email
 
 st.header("Page 2")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+
+    if button:
+        send_email()
--- showcase_website/send_emails.py
+++ showcase_website/send_emails.py
@@ -1,16 +1,17 @@
 import  smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email():
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "test@test.com"
-password = "1234"
+    username = "test@test.com"
+    password = "1234"
 
-receiver = "test2@test.com"
-context = ssl.create_default_context()
+    receiver = "test2@test.com"
+    context = ssl.create_default_context()
 
-message =""" Hi """
+    message =""" Hi """
 
-with smtplib.SMTP_SSL(host, port, context=context) as sever:
-    sever.login(username, password)
-    sever.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as sever:
+        sever.login(username, password)
+        sever.sendmail(username, receiver, message)
